2e1b63cbe36b1f8b3249ad34597b5736a94e430e
flalini <flan101544@gmail.com>
2023-10-01 23:16:02 +0900

    Rename Equment to Equipment
    fix typo

---
+++ VSOnTheRecord/OnTheRecord/BasicComponent/Equipment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+  미완 이런 느낌이다 참고만 할 것
+*/
+namespace OnTheRecord.BasicComponent
+{
+	class Equipment
+	{
+	}
+
+	class Uequ
+	{
+		//현재 장착되어 있는 Equment 집합체
+		//to do Equment 추가, 제거 교체 method
+		//내부의 Equment들이 가진 스탯 가중치를 계산해서 하나의 스탯으로 만들 기능이 필요함
+		//내부의 Equment들이 가진 스킬들을 특정 스킬 리스트(착용자) 에게 반영해주는 기능이 필요함
+	}
+}
