48f472c2191dfb35cb6fe714d38f8eaf30c4208a
flalini <flan101544@gmail.com>
2023-11-28 22:11:46 +0900

    Update ActivableList.cs

--- VSOnTheRecord/OnTheRecord/Entity/ActivableList.cs
+++ VSOnTheRecord/OnTheRecord/Entity/ActivableList.cs
@@ -6,32 +6,61 @@ using System.Threading.Tasks;
 using OnTheRecord.BasicComponent;
 using OnTheRecord.Map;
 using OnTheRecord.Entity;
+using ExternalStaticReference;
 
 namespace OnTheRecord.Entity
 {
 	public class ActivableList
 	{
 		public List<Activable> activableList;
+		public int currentTurnActivable;
+
+		public void Situation(int situation)
+		{
+			switch (situation)
+			{
+				case (int)SituationCode.endTurn:
+					TurnEnd();
+					break;
+				case (int)SituationCode.endRound:
+					RoundEnd();
+					break;
+			}
+		}
+
+		private void TurnEnd()
+		{
+			currentTurnActivable++;
+		}
+
+		private void RoundEnd()
+		{
+			currentTurnActivable = 0;
+		}
 
 		public ActivableList()
 		{
 			activableList = new List<Activable>();
+			currentTurnActivable = 0;
 		}
 
 		public ActivableList(List<Activable> activableList)
 		{
 			this.activableList = activableList;
+			currentTurnActivable = 0;
 		}
 
 		public ActivableList(Activable activable)
 		{
 			activableList = new List<Activable>();
 			activableList.Add(activable);
+			currentTurnActivable = 0;
 		}
 
 		public ActivableList(ActivableList activableList)
 		{
 			this.activableList = activableList.activableList;
+			currentTurnActivable = 0;
 		}
 
 		public ActivableList(List<Player> playerList)
@@ -41,27 +70,43 @@ namespace OnTheRecord.Entity
 			{
 				activableList.Add(player);
 			}
+			currentTurnActivable = 0;
 		}
 
-		public AddActivable(Activable activable)
+		public void AddActivable(Activable activable)
 		{
 			activableList.Add(activable);
 		}
 
-		public AddActivable(ActivableList activableList)
+		public void AddActivable(ActivableList activableList)
 		{
-			foreach (Activable activable in activableList)
+			foreach (Activable activable in activableList.activableList)
 			{
-				activableList.Add(activable);
+				this.activableList.Add(activable);
 			}
 		}
 
-		public AddActivable(List<Player> playerList)
+		public void AddActivable(List<Player> playerList)
 		{
 			foreach (Player player in playerList)
 			{
 				activableList.Add(player);
 			}
+		}
+
+		public void RemoveActivable(Activable activable)
+		{
+			activableList.Remove(activable);
+		}
+
+		public void RemoveAtActivable(int index)
+		{
+			activableList.RemoveAt(index);
+		}
+
+		public void SortTurn()
+		{
+			activableList.Sort()
 		}
 	}
 }
